87f8f8847fb35af3dca81fe0f9378d29a7653d44
Merlin <158784988+merlinz01@users.noreply.github.com>
2025-12-10 22:08:54 +0000

    Cherry pick of 8656f196056ea7212c27c07bf0c2957f6139e7f7 failed
    stdout:
    Auto-merging addons/payment/models/res_country.py
    CONFLICT (content): Merge conflict in addons/payment/models/res_country.py
    
    stderr:

--- addons/payment/models/res_country.py
+++ addons/payment/models/res_country.py
@@ -1,8 +1,17 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+<<<<<<< 07b49e0657f0dbcdf2505fb7f323c617d1adad06
 
 import odoo.addons.payment_stripe as stripe  # prevent circular import error with payment_stripe
+||||||| 30cbd52a5c0d0a911648dcb45143a4ec551f606c
+import odoo.addons.payment_stripe as stripe  # prevent circular import error with payment_stripe
+=======
+try:
+    import odoo.addons.payment_stripe as stripe  # prevent circular import error with payment_stripe
+except ModuleNotFoundError:
+    stripe = None
+>>>>>>> 4d9ae49ca244fcdd2194a40ebc2043d0e163c7e2
 
 
 class ResCountry(models.Model):
@@ -13,6 +22,9 @@ class ResCountry(models.Model):
     @api.depends('code')
     def _compute_is_stripe_supported_country(self):
         for country in self:
-            country.is_stripe_supported_country = stripe.const.COUNTRY_MAPPING.get(
-                country.code, country.code
-            ) in stripe.const.SUPPORTED_COUNTRIES
+            country.is_stripe_supported_country = (
+                stripe is not None
+                and stripe.const.COUNTRY_MAPPING.get(
+                    country.code, country.code
+                ) in stripe.const.SUPPORTED_COUNTRIES
+            )
